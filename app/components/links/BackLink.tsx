13df1e3c111b45c7530a77367d22b2aa6ca2d9de
m2rt <mart@bitweb.ee>
2024-09-17 10:17:33 +0300

    BackLink fix

--- app/components/links/BackLink.tsx
+++ app/components/links/BackLink.tsx
@@ -11,7 +11,13 @@ export default function BackLink({ linkToHomePage }: Readonly<BackLinkProps>) {
         window.history.back();
     }
 
-    if (linkToHomePage || !document.referrer.includes(location.origin)) {
+    let isSamePage = false;
+
+    if (typeof window !== "undefined") {
+        isSamePage = window.document.referrer.includes(location.origin);
+    }
+
+    if (linkToHomePage || !isSamePage) {
         return (
             <Link href="/" className="flex items-center gap-1 hover:underline">
                 <ArrowLeftIcon className="w-4 h-4 -mt-1" /> Avalehele
