5db6a27f8e6db31c65929d751e7b2c92b989f3a6
m2rt <andra0401@hotmail.com>
2024-10-02 11:39:36 +0300

    Fixed multiple h1 error

--- app/components/homepage/Hero.tsx
+++ app/components/homepage/Hero.tsx
@@ -31,7 +31,7 @@ const Hero = ({ events }: Readonly<CalendarProps>) => {
                     </div>
                     <div className='bg-dark p-12 md:p-16 xl:p-20 px-4 flex flex-col rounded-tr-md lg:rounded-tr-lg rounded-bl-md lg:rounded-bl-lg -mb-20 mt-4 shadow-3xl'>
                         <h1 className='text-white text-center text-xl md:text-2xl lg:text-4xl font-light'>Praktilise suunitlusega koolituskeskus</h1>
-                        <h1 className='text-white text-center text-2xl md:text-4xl lg:text-5xl font-display'>sotsiaaltöö spetsialistidele</h1>
+                        <h2 className='text-white text-center text-2xl md:text-4xl lg:text-5xl font-display'>sotsiaaltöö spetsialistidele</h2>
                     </div>
 
                 </div>
